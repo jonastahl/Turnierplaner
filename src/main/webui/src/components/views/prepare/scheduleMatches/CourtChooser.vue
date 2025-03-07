026ad18167176bffc9aff659f2a6b9bb4ed87e42
Oliver Fung <53515503+FungOliver@users.noreply.github.com>
2025-03-08 00:43:16 +0100

    Revert "🌐 Internalize select courts"
    This reverts commit b7d3ec6f8b202fdb62d1edb23ed332b417397fcc.

--- src/main/webui/src/components/views/prepare/scheduleMatches/CourtChooser.vue
+++ src/main/webui/src/components/views/prepare/scheduleMatches/CourtChooser.vue
@@ -4,7 +4,7 @@
 		:loading="!courts || !selectedCourts"
 		:options="courts?.toSorted(courtComp)"
 		option-label="name"
-		:placeholder="t('court.select')"
+		placeholder="Select courts"
 		class="w-full"
 	>
 		<template #footer>
@@ -20,9 +20,6 @@ import ViewCreateCourtSmall from "@/components/views/court/ViewCreateCourtSmall.
 import { Court } from "@/interfaces/court"
 import { courtComp, getCourts } from "@/backend/court"
 import { watch } from "vue"
-import { useI18n } from "vue-i18n"
-
-const { t } = useI18n()
 
 const selectedCourts = defineModel<Court[]>({ default: [] })
 const { data: courts } = getCourts()
