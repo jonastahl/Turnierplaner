2e7f0e5ab49dbd1823dc0ac68b08db46c8cd571f
Oliver Fung <53515503+FungOliver@users.noreply.github.com>
2025-03-08 00:43:16 +0100

    Revert "🔧 Don't use # in routes"
    This reverts commit d3766ef063a2f99766c53b0fcccbe15549305d7f.

--- src/main/webui/src/main.ts
+++ src/main/webui/src/main.ts
@@ -6,6 +6,7 @@ import * as VueRouter from "vue-router"
 import { settings } from "@/settings"
 
 /* https request */
+import VueAxios from "vue-axios"
 import axios from "axios"
 
 import { access_token } from "@/security/AuthService"
@@ -78,7 +79,7 @@ declare module "@vue/runtime-core" {
 const app = createApp(App)
 
 const router = VueRouter.createRouter({
-	history: VueRouter.createWebHistory(),
+	history: VueRouter.createWebHashHistory(),
 	routes,
 })
 
@@ -111,7 +112,7 @@ const vueQueryPluginOptions: VueQueryPluginOptions = {
 
 app
 	.use(i18n)
-	.use(axios)
+	.use(VueAxios, axios)
 	.use(router)
 	/* Primevue */
 	.use(PrimeVue, { ripple: true })
