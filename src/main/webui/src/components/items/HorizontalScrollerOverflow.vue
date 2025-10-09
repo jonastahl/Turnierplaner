862be5214ebc1fba4ae6c72f98e0d0f29e430ad3
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2025-10-09 23:45:51 +0200

    🎨 Format

--- src/main/webui/src/components/items/HorizontalScrollerOverflow.vue
+++ src/main/webui/src/components/items/HorizontalScrollerOverflow.vue
@@ -1,15 +1,11 @@
 <template>
 	<div class="w-full overflow-x-auto">
-		<div
-			class="inline-block flex-wrap w-fit"
-			style="min-width: 100%"
-		>
+		<div class="inline-block flex-wrap w-fit" style="min-width: 100%">
 			<slot></slot>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
