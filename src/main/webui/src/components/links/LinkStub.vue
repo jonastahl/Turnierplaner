4bd9fe16be7696e7f2c25af8028166c39da1f420
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2026-07-13 10:39:46 +0200

    🐛 Use router-link for links

---
+++ src/main/webui/src/components/links/LinkStub.vue
@@ -0,0 +1,22 @@
+<template>
+	<router-link
+		v-if="routeTarget"
+		class="cursor-pointer hover:underline no-underline text-color"
+		:to="routeTarget"
+	>
+		<slot></slot>
+	</router-link>
+	<span v-else>
+		<slot></slot>
+	</span>
+</template>
+
+<script setup lang="ts">
+import { RouteLocationRaw } from "vue-router"
+
+defineProps<{
+	routeTarget: RouteLocationRaw | null
+}>()
+</script>
+
+<style scoped></style>
