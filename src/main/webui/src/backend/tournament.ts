c6c517408c41e483ed064171e24c7fbd6c8e4198
Oliver Fung <oliverfung.online@gmx.de>
2026-07-15 22:37:59 +0200

    ♻️ remove commented-out queryClient.removeQueries calls for competition data

--- src/main/webui/src/backend/tournament.ts
+++ src/main/webui/src/backend/tournament.ts
@@ -164,12 +164,6 @@ export function useDeleteTournament(
 			queryClient.removeQueries({
 				queryKey: ["tournament", tourId],
 			})
-			// queryClient.removeQueries({
-			// 	queryKey: ["competitionList", tourId],
-			// })
-			// queryClient.removeQueries({
-			// 	queryKey: ["competitionDetails", tourId],
-			// })
 			queryClient
 				.invalidateQueries({
 					queryKey: ["tournamentList"],
